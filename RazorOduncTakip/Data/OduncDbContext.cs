357db3b5d7c411fb9020e8d6ffa06cf769fb8581
Selkan Larlar Trainer <sl@trainer.com>
2022-12-07 14:06:39 +0300

    DbContext , OduncIslem model tamamlandı

---
+++ RazorOduncTakip/Data/OduncDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorOduncTakip.Model;
+
+namespace RazorOduncTakip.Data
+
+{
+    public class OduncDbContext :DbContext
+    {
+        public DbSet<OduncIslem> OduncIslemler { get; set; }
+        public OduncDbContext(DbContextOptions<OduncDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ RazorOduncTakip/Model/OduncIslem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorOduncTakip.Model
+{
+    public class OduncIslem
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime DateGive { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime DateReturn { get; set; }
+    }
+}
