9acac0236badc11b52d59f1bf7fb7c9703ef4e88
Selkan Larlar Trainer <sl@trainer.com>
2022-12-07 14:20:23 +0300

    Takip index , create sayfalar eklenid

---
+++ RazorOduncTakip/Pages/OduncTakip/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorOduncTakip.Pages.OduncTakip
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ RazorOduncTakip/Pages/OduncTakip/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorOduncTakip.Pages.OduncTakip
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
