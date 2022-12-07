e820308523cb71a267e0078908ec85644098d6ff
Selkan Larlar Trainer <sl@trainer.com>
2022-12-07 14:28:54 +0300

    Takip index model, page tamamlandı

--- RazorOduncTakip/Pages/OduncTakip/Index.cshtml.cs
+++ RazorOduncTakip/Pages/OduncTakip/Index.cshtml.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorOduncTakip.Data;
+using RazorOduncTakip.Model;
 
 namespace RazorOduncTakip.Pages.OduncTakip
 {
     public class IndexModel : PageModel
     {
+        public IEnumerable<OduncIslem> OduncIslemler { get; set; }
+        private readonly OduncDbContext _db;
+        public IndexModel(OduncDbContext db)
+        {
+            _db = db;
+        }
         public void OnGet()
         {
+            OduncIslemler = _db.OduncIslemler;
         }
     }
 }
