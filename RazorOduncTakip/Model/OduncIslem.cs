6a768a0b4003f3c7c21d833513a62ee6f017b4f3
Selkan Larlar Trainer <sl@trainer.com>
2022-12-07 14:56:54 +0300

    Creat Page akış tamamlandı

--- RazorOduncTakip/Model/OduncIslem.cs
+++ RazorOduncTakip/Model/OduncIslem.cs
@@ -5,11 +5,15 @@ namespace RazorOduncTakip.Model
     public class OduncIslem
     {
         public int Id { get; set; }
+        
         [Required]
         public string Name { get; set; }
+        
         [Required]
         [DataType(DataType.DateTime)]
+        [Display(Name = "Date Given")]
         public DateTime DateGive { get; set; }
+        
         [DataType(DataType.DateTime)]
         public DateTime DateReturn { get; set; }
     }
--- RazorOduncTakip/Pages/OduncTakip/Create.cshtml.cs
+++ RazorOduncTakip/Pages/OduncTakip/Create.cshtml.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorOduncTakip.Model;
 
 namespace RazorOduncTakip.Pages.OduncTakip
 {
     public class CreateModel : PageModel
     {
+        [BindProperty]
+        public OduncIslem OduncIslem { get; set; }
+
         public void OnGet()
         {
+
         }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage("Index");
+        }
+
+
     }
 }
