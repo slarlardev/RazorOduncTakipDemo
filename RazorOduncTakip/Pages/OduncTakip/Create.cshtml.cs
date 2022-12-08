eaf28c5efdefdcfa8b6e80275c371965c884fe69
Selkan Larlar Trainer <sl@trainer.com>
2022-12-08 13:41:41 +0300

    Return sayfası tamamlandı

--- RazorOduncTakip/Pages/OduncTakip/Create.cshtml.cs
+++ RazorOduncTakip/Pages/OduncTakip/Create.cshtml.cs
@@ -2,11 +2,19 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RazorOduncTakip.Model;
 using RazorOduncTakip.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace RazorOduncTakip.Pages.OduncTakip
 {
     public class CreateModel : PageModel
     {
+        public List<SelectListItem> Items { get; } = new List<SelectListItem>
+        {
+            new SelectListItem { Value= "Bilgisayar", Text="Bilgisayar" },
+            new SelectListItem { Value= "Video Kamera", Text="Video Kamera" },
+            new SelectListItem { Value= "Kitap 101", Text="Kitap 101" }
+        };
+
         private readonly OduncDbContext db;
 
         [BindProperty]
--- RazorOduncTakip/Pages/OduncTakip/Return.cshtml.cs
+++ RazorOduncTakip/Pages/OduncTakip/Return.cshtml.cs
@@ -1,12 +1,48 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorOduncTakip.Data;
+using RazorOduncTakip.Model;
+
 
 namespace RazorOduncTakip.Pages.OduncTakip
 {
     public class ReturnModel : PageModel
     {
-        public void OnGet()
+        //[BindProperty(SupportsGet = true)]
+        //public int Id { get; set; }
+
+        [BindProperty]
+        public OduncIslem OduncIslem { get; set; }
+
+        private readonly OduncDbContext db;
+        public ReturnModel(OduncDbContext db)
         {
+            this.db = db;
         }
+        public void OnGet(int id)
+        {
+            OduncIslem = db.OduncIslemler.Find(id);
+        }
+
+        public IActionResult OnPost()
+        {
+            // iþ kuralý iade tarihi veriliþ tarihinden önce olamaz
+            if (OduncIslem.DateReturn is not null  &&
+                DateTime.Compare(OduncIslem.DateGive, (DateTime)OduncIslem.DateReturn) > 0)
+            {
+                ModelState.AddModelError("@OduncIslem.DateReturn", "iade tarihi veriliþ tarihinden önce olamaz");
+            }
+            // model valid?
+            if (ModelState.IsValid)
+            {
+                db.OduncIslemler.Update(OduncIslem);
+                db.SaveChanges();
+                return RedirectToPage("index");
+            }
+
+            return Page();
+
+        }
+
     }
 }
